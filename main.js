ae9a7bbf3b06b036cb337c882b79887385164a65
Harshul Nanwani <72592254+Harshul26Nanwani@users.noreply.github.com>
2023-11-27 16:21:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Machine Learning", "Data Engineer", "Cloud Security and Architecture"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+  
